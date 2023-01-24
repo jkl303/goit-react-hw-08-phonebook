d8e47edc11b96eecae32e1f679398f8acd357ed1
jkl303 <103362175+jkl303@users.noreply.github.com>
2023-01-24 13:22:48 +0000

    Deploying to gh-pages from @ jkl303/goit-react-hw-08-phonebook@5e86a0585693da3938f6511c501bc36a7ae74a3a 🚀

---
+++ static/js/427.e643b679.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[427],{427:function(n,e,o){o.r(e),o.d(e,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)("div",{children:"Login"})}}}]);
+//# sourceMappingURL=427.e643b679.chunk.js.map
---
+++ static/js/544.ebbd0247.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{544:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=544.ebbd0247.chunk.js.map
