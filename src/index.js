600cbe9d202818d1bbb0c947e92c57b850c5c920
jkl303 <303vladzoz@gmail.com>
2023-02-02 13:40:21 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { HelmetProvider } from 'react-helmet-async';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <PersistGate loading={null} persistor={persistor}>
           <HelmetProvider>
             <App />
